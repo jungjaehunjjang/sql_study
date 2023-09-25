b99ca2bfbf1b80e346913fb844a31fad0e04a6b3
jaehun <josh9269@naver.com>
2023-09-25 17:31:59 +0900

    sql_study 0925

--- 02_WHERE절.sql
+++ 02_WHERE절.sql
@@ -1,7 +1,7 @@
 
 SELECT * FROM employees;
 
---WHERE절 비교 (데이터 값은 대/소문자를 구분합니다.)
+-- WHERE절 비교 (데이터 값을 대/소문자를 구분합니다.)
 SELECT first_name, last_name, job_id
 FROM employees
 WHERE job_id = 'IT_PROG';
@@ -26,45 +26,42 @@ SELECT * FROM employees
 WHERE salary BETWEEN 15000 AND 20000;
 
 SELECT * FROM employees
-WHERE hire_date BETWEEN '03/01/01' AND '03/12/31';
+WHERE hire_date BETWEEN '03/01/01' AND '03/12/31'; 
 
--- IN 연산자의 사용 (특정 값들과 비교할 때 사용)
+-- IN 연산자의 사용(특정 값들과 비교할 때 사용)
 SELECT * FROM employees
-WHERE manager_id IN (100,101,102);
+WHERE manager_id IN (100, 101, 102);
 
 SELECT * FROM employees
-WHERE job_id IN ('IT_PORG', 'AD_VP');
+WHERE job_id IN ('IT_FROG', 'AD_VP');
 
 -- LIKE 연산자
 -- %는 어떠한 문자든, _는 데이터의 자리(위치)를 찾아낼 때
 SELECT first_name, last_name, hire_date
 FROM employees
-WHERE hire_date LIKE '03%'; -- 앞에03
+WHERE hire_date LIKE '03%'; --앞에 03이 있는 것
 
 SELECT first_name, hire_date
 FROM employees
-WHERE hire_date LIKE '%15'; -- 뒤에15
+WHERE hire_date LIKE '%15'; --끝에 15가 있는 것
 
 SELECT first_name, hire_date
 FROM employees
-WHERE hire_date LIKE '%05%';-- 어디든 05
+WHERE hire_date LIKE '%05%'; --05가 있으면 전부
 
 SELECT first_name, hire_date
 FROM employees
-WHERE hire_date LIKE '___05%'; --_ 문자하나당 >>4번째숫자가0 5
+WHERE hire_date LIKE '___12%'; -- _문자 하나 당
 
 -- IS NULL (null값을 찾음)
 SELECT * FROM employees
-WHERE manager_id IS NULL;
-
-SELECT * FROM employees
-WHERE commission_pct IS NULL;
+WHERE manager_id IS NULL; 
 
 SELECT * FROM employees
 WHERE commission_pct IS NOT NULL;
 
 -- AND, OR
--- AND가 OR보다 연산 순서가 빠름.
+-- AND가 OR보다 연산 순서가 빠름. 
 SELECT * FROM employees
 WHERE job_id = 'IT_PROG'
 OR job_id = 'FI_MGR'
@@ -76,12 +73,12 @@ OR job_id = 'FI_MGR')
 AND salary >= 6000;
 
 -- 데이터의 정렬 (SELECT 구문의 가장 마지막에 배치됩니다.)
--- ASC: ascending 오름차순 (생략가능)
+-- ASC: ascending 오름차순
 -- DESC: descending 내림차순
-SELECT *FROM employees
-ORDER BY hire_date ASC;
+SELECT * FROM employees
+ORDER BY hire_date ASC; --ASC를 생략하면 기본값이 된다.
 
-SELECT *FROM employees
+SELECT * FROM employees
 ORDER BY hire_date DESC;
 
 SELECT * FROM employees
@@ -94,7 +91,7 @@ ORDER BY employee_id DESC;
 
 SELECT 
     first_name,
-    salary*12 AS pay
+    salary*12 As pay
 FROM employees
 ORDER BY pay ASC;
 
--- 04_문자열함수.sql
+++ 04_문자열함수.sql
@@ -11,8 +11,8 @@ dual이라는 테이블은 sys가 소유하는 오라클의 표준 테이블로서,
 */
 
 SELECT
-    'abcDEF', lower('abcDEF'), upper('abcDEF')
-FROM
+ 'abcDEF', lower('abcDEF'), upper('abcDEF')
+ FROM
     dual;
     
 SELECT
@@ -25,20 +25,21 @@ FROM employees;
 SELECT last_name FROM employees
 WHERE LOWER(last_name) = 'austin';
 
---length(길이), instr(문자 찾기, 없으면 0을 반환, 있으면 인덱스 값)
+-- length(길이), instr(문자 찾기, 없으면 0을 반환, 있으면 인덱스 값)
 SELECT
-    'abcdef', LENGTH('abcdef'), INSTR('abcdef', 'z')
+    'abcDEF', LENGTH('abcDEF'), INSTR('abcDEF', 'z')
 FROM dual;
 
 SELECT
     first_name, LENGTH(first_name), INSTR(first_name, 'a')
 FROM employees;
 
---substr(자를 문자열, 시작 인텍스, 길이), concat(문자 연결)
---substr(자를 문자열, 시작 인덱스) -> 문자열 끝까지.
+-- substr(자를 문자열, 시작 인덱스, 길이), concat(문자 연결)
+-- substr(자를 문자열, 시작 인덱스) -> 문자열 끝까지
+-- 인덱스 1부터 시작
 SELECT
-    'abcdef' AS ex,
-    SUBSTR('abcdef', 1, 4),
+    'abcdef' As ex,
+    SUBSTR('abcdef', 2, 3),
     CONCAT('abc', 'def')
 FROM dual;
 
@@ -48,55 +49,52 @@ SELECT
     CONCAT(first_name, last_name)
 FROM employees;
 
---LPAD, RPAD(좌, 우측 지정 문자열로 채우기)
+-- LAPD, RPAD(좌, 우측 지정 문자열로 채우기)
 SELECT
     LPAD('abc', 10, '*'),
     RPAD('abc', 10, '*')
 FROM dual;
 
---LTRIM(), RTRIM(), TRIM() 공백 제거
---LTRIM(param1, param2) -> param2의 값을 param1에서 찾아서 제거.(왼쪽부터)
---RTRIM(param1, param2) -> param2의 값을 param1에서 찾아서 제거.(오른쪽부터)
-
+-- LTRIM(), RTRIM(), TRIM() 공백 제거
+-- LTRIM(param1, param2) -> param2의 값을 param1에서 찾아서 제거, (왼쪽부터)
+-- RTRIM(param1, param2) -> param2의 값을 param1에서 찾아서 제거, (오른쪽부터)
 SELECT LTRIM('javascript_java', 'java') FROM dual;
-SELECT RTRIM('javascript_java', 'java') FROM dual;
-SELECT TRIM('     java     ')FROM dual;
+SELECT RTRIM('javascript_java', 'java') FROM dual; 
+SELECT TRIM('   java    ') FROM dual;
 
--- replace()
+--replace() 
 SELECT
-    REPLACE('My dream is president', 'president', 'programmer')
+    REPLACE('MY dream is a president', 'president', 'programmer')
 FROM dual;
 
 SELECT
-    REPLACE(REPLACE('My dream is president', 'president', 'programmer'), ' ', '')
+    REPLACE(REPLACE('MY dream is a president', 'president', 'programmer'), ' ', '')
 FROM dual;
 
 SELECT
     REPLACE(CONCAT('hello', ' world!'), '!', '?')
 FROM dual;
 
-
-
 /*
 문제 1.
-EMPLOYEES 테이블에서 이름, 입사일자 컬럼으로 변경(별칭)해서 이름순으로 오름차순 출력 합니다.
-조건 1) 이름 컬럼은 first_name, last_name을 붙여서 출력합니다.(CONCAT)
-조건 2) 입사일자 컬럼은 xx/xx/xx로 저장되어 있습니다. xxxxxx형태로 변경해서 출력합니다.(REPLACE)
+EMPLOYEES 테이블에서 이름, 입사일자 컬럼으로 변경해서 이름순으로 오름차순 출력 합니다.(hire_date를 입사일자로)
+조건 1) 이름 컬럼은 first_name, last_name을 붙여서 출력합니다. (concat)
+조건 2) 입사일자 컬럼은 xx/xx/xx로 저장되어 있습니다. xxxxxx형태로 변경해서 출력합니다. (replace)
 */
 SELECT
-    CONCAT(first_name, last_name) AS 이름,
-    REPLACE(hire_date, '/', '') AS 입사일자
+    CONCAT(first_name, last_name) AS 이름, REPLACE(hire_date, '/', '') AS 입사일자
 FROM employees;
 
 /*
-문제 2.
+문제 2.                                 
 EMPLOYEES 테이블에서 phone_number컬럼은 ###.###.####형태로 저장되어 있다
-여기서 처음 세 자리 숫자 대신 서울 지역변호 (02)를 붙여 
-전화 번호를 출력하도록 쿼리를 작성하세요. (CONCAT, SUBSTR,사용)
+여기서 처음 세 자리 숫자 대신 서울 지역변호 (02)를 붙여 (02).###.####  02#######
+전화 번호를 출력하도록 쿼리를 작성하세요. (CONCAT, SUBSTR,)
 */
 SELECT
-    REPLACE(CONCAT('(02)',(SUBSTR(phone_number,3))),'.','') AS phone_number
+    CONCAT('(02)',SUBSTR(phone_number, 4))
 FROM employees;
+
 /*
 문제 3. 
 EMPLOYEES 테이블에서 JOB_ID가 it_prog인 사원의 이름(first_name)과 급여(salary)를 출력하세요.
@@ -107,9 +105,8 @@ EMPLOYEES 테이블에서 JOB_ID가 it_prog인 사원의 이름(first_name)과 급여(salary)를
 이 열의 열 별칭은 salary입니다.(힌트 : lpad 이용)
 */
 SELECT
-    (RPAD(RPAD(first_name,3,'*'),10,'*'))AS name,
-    LPAD(salary,10,'*')AS salary
+     RPAD(SUBSTR(first_name, 1,3),10,'*') As name,
+     LPAD(salary,10,'*')
 FROM employees
-WHERE LOWER(job_id) ='it_prog';
-
+WHERE LOWER(JOB_ID) ='it_prog';
 
