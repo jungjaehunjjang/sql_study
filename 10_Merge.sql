8bea29d3423488cd157dce4d672ab25870920274
jaehun <josh9269@naver.com>
2023-10-17 09:24:27 +0900

    10.17 sql_study 추가

--- 10_Merge.sql
+++ 10_Merge.sql
@@ -13,13 +13,14 @@ INSERT INTO emps_it
     (employee_id, first_name, last_name, email, hire_date, job_id)
 VALUES
     (106, '춘식', '김', 'CHOONSIK', sysdate, 'IT_PROG');
-
+    
 SELECT * FROM emps_it;
 
 SELECT * FROM employees
 WHERE job_id = 'IT_PROG';
 
-MERGE INTO emps_it a  --(머지를 할 타겟 테이블)
+
+MERGE INTO emps_it a -- (머지를 할 타겟 테이블)
     USING -- 병합시킬 데이터
         (SELECT * FROM employees
          WHERE job_id = 'IT_PROG') b -- 병합하고자 하는 데이터를 서브쿼리로 표현.
@@ -34,7 +35,7 @@ WHEN MATCHED THEN -- 조건이 일치하는 경우에는 타겟 테이블에 이렇게 실행하라.
         a.manager_id = b.manager_id,
         a.department_id = b.department_id
         
-         /*
+        /*
         DELETE만 단독으로 쓸 수는 없습니다.
         UPDATE 이후에 DELETE 작성이 가능합니다.
         UPDATE 된 대상을 DELETE 하도록 설계되어 있기 때문에
@@ -44,16 +45,14 @@ WHEN MATCHED THEN -- 조건이 일치하는 경우에는 타겟 테이블에 이렇게 실행하라.
     DELETE
         WHERE a.employee_id = b.employee_id
         
-
-        
 WHEN NOT MATCHED THEN
     INSERT /*속성(컬럼)*/ VALUES 
     (b.employee_id, b.first_name, b.last_name,
     b.email, b.phone_number, b.hire_date, b.job_id,
     b.salary, b.commission_pct, b.manager_id, b.department_id);
+    
 
-
----------------------------------------------------------------------
+-------------------------------------------------------------------------------
 
 INSERT INTO emps_it
     (employee_id, first_name, last_name, email, hire_date, job_id)
@@ -90,66 +89,130 @@ WHEN MATCHED THEN
 WHEN NOT MATCHED THEN
     INSERT VALUES
     (b.employee_id, b.first_name, b.last_name,
-     b.email, b.phone_number, b.hire_date, b.job_id,
-     b.salary, b.commission_pct, b.manager_id, b.department_id);
-     
+    b.email, b.phone_number, b.hire_date, b.job_id,
+    b.salary, b.commission_pct, b.manager_id, b.department_id);
+
 SELECT * FROM emps_it
 ORDER BY employee_id ASC;
 
 ROLLBACK;
 
+--------------------------------------------------------------------------------
 
---문제 1
+CREATE TABLE depts AS (SELECT * FROM departments);
 SELECT * FROM depts;
 
-DROP TABLE depts;
-CREATE TABLE depts AS(SELECT * FROM departments);
-INSERT INTO depts
-    (department_id, department_name, manager_id, location_id)
-VALUES
-    (280, '개발', null, 1800);
-INSERT INTO depts
-    (department_id, department_name, manager_id, location_id)
-VALUES
-    (290, '회계부', null, 1800);
-INSERT INTO depts
-    (department_id, department_name, manager_id, location_id)
-VALUES
-    (300, '재정', 301, 1800);
-INSERT INTO depts
-    (department_id, department_name, manager_id, location_id)
-VALUES
-    (310, '인사', 302, 1800);
-INSERT INTO depts
-    (department_id, department_name, manager_id, location_id)
-VALUES
-    (320, '영업', 303, 1700);
+-- 문제 1
+INSERT INTO depts (department_id, department_name, location_id)
+VALUES(280, '개발', 1800);
 
---문제2-1
-UPDATE depts SET department_name = 'IT bank'
+INSERT INTO depts (department_id, department_name, location_id)
+VALUES(290, '회계부', 1800);
+
+INSERT INTO depts 
+VALUES(300, '재정', 301, 1800);
+
+INSERT INTO depts 
+VALUES(310, '인사', 302, 1800);
+
+INSERT INTO depts 
+VALUES(320, '영업', 303, 1700);
+
+SELECT * FROM depts;
+
+-- 문제 2
+-- 2-1
+UPDATE depts SET department_name = 'IT_BANK'
 WHERE department_name = 'IT Support';
 
---문제2-2
+-- 2-2
 UPDATE depts SET manager_id = 301
 WHERE department_id = 290;
 
---문제2-3
-UPDATE depts SET department_name = 'IT Help',
-                    manager_id =303,
-                    location_id = 1800
+-- 2-3
+UPDATE depts
+SET
+    department_name = 'IT Help',
+    manager_id = 303,
+    location_id = 1800
 WHERE department_name = 'IT Helpdesk';
 
---문제2-4
---회계, 재정, 인사, 영업부의 매니저 아이디를 301로 일괄 변경하세요.
-UPDATE depts SET manager_id = 301
-WHERE department_name IN ('회계부', '재정', '인사', '영업');
+-- 2-4
+UPDATE depts
+SET
+    manager_id = 301
+WHERE department_id IN (290, 300, 310, 320); 
 
---문제3-1
-DELETE FROM depts
-WHERE department_name = '영업';
+SELECT * FROM depts;
 
---문제3-2
-DELETE FROM depts
-WHERE department_name = 'NOC';
+-- 문제 3
+DELETE FROM depts WHERE department_id = (SELECT department_id FROM depts 
+                                         WHERE department_name = '영업');
+
+DELETE FROM depts WHERE department_id = (SELECT department_id FROM depts 
+                                         WHERE department_name = 'NOC');
+
+-- 문제 4
+-- 4-1
+DELETE FROM depts WHERE department_id > 200;
+
+-- 4-2
+UPDATE depts 
+SET manager_id = 100
+WHERE manager_id IS NOT NULL;
+
+SELECT * FROM depts;
+SELECT * FROM departments;
+
+-- 4-4
+MERGE INTO depts a
+    USING (SELECT * FROM departments) d
+    ON (a.department_id = d.department_id)
+WHEN MATCHED THEN
+    UPDATE SET
+    a.department_name = d.department_name,
+    a.manager_id = d.manager_id,
+    a.location_id = d.location_id
+
+WHEN NOT MATCHED THEN
+    INSERT VALUES
+    (d.department_id, d.department_name, d.manager_id, d.location_id);
+
+-- 문제 5
+-- 5-1
+CREATE TABLE jobs_it AS
+(SELECT * FROM jobs WHERE min_salary > 6000);
+
+SELECT * FROM jobs_it;
+SELECT * FROM jobs;
+
+-- 5-2
+INSERT INTO jobs_it VALUES('IT_DEV', '아이티개발팀', 6000, 20000);
+INSERT INTO jobs_it VALUES('NET_DEV', '네트워크개발팀', 5000, 20000);
+INSERT INTO jobs_it VALUES('SEC_DEV', '보안개발팀', 6000, 19000);
+
+-- 5-4
+MERGE INTO jobs_it a
+    USING (SELECT * FROM jobs WHERE min_salary > 5000) b
+    ON (a.job_id = b.job_id)
+WHEN MATCHED THEN
+    UPDATE SET
+    a.min_salary = b.min_salary,
+    a.max_salary = b.max_salary
+    
+WHEN NOT MATCHED THEN
+    INSERT VALUES
+    (b.job_id, b.job_title, b.min_salary, b.max_salary);
+    
+SELECT * FROM jobs_it;
+
+
+
+
+
+
+
+
+
 
 
